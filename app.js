0c84c4bcfc0f14a2d1195c6c12e73c97bc54e179
Nabin Singh <nabin@skilluplabs.com.au>
2025-01-06 19:09:57 +1100

    Add show friends function

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 let unsettledAmount = 120;
+let friends = [
+  {
+    firstName: 'Alex',
+    lastName: 'Lee',
+    mobile: '0420400163',
+    email: 'alex.lee@gmail.com',
+  },
+  {
+    firstName: 'Bob',
+    lastName: 'Smith',
+    mobile: '0420400183',
+    email: 'bob.smith@gmail.com',
+  },
+  {
+    firstName: 'Charlie',
+    lastName: 'Trump',
+    mobile: '0420400283',
+    email: 'charlie.trump@gmail.com',
+  },
+];
 
+// Function to display friends
+function showFriends(friends) {
+  const friendsElement = document.getElementById('friends');
+  let friendListElements = '';
+  for (let friend of friends) {
+    let friendElement = `<div title="${friend.firstName} ${friend.lastName}"></div>`;
+    friendListElements = friendListElements + friendElement;
+  }
+  friendsElement.innerHTML = friendListElements;
+}
+
+// Function to show unsettled amount
 function showUnsettledAmount(unsettledAmount) {
   const unsettledAmountElement = document.getElementById('unsettled_amount');
   unsettledAmountElement.innerHTML = unsettledAmount;
 }
 
+showFriends(friends);
 showUnsettledAmount(150);
